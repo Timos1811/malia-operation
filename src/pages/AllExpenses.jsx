a577d3d759a6a77df7529155119a88e54ec82142
Tim Shlafer <shtim.work@gmail.com>
2026-01-30 13:00:52 +0000

    File changes

--- src/pages/AllExpenses.jsx
+++ src/pages/AllExpenses.jsx
@@ -248,7 +248,7 @@ export default function AllExpenses() {
                         </td>
                     );})}
                   </tr>
-                ))}
+                );})}
               </tbody>
             </table>
           )}
