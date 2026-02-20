0aabdc9d9025f44ec15ef033b8784e01679e5501
Tim Shlafer <shtim.work@gmail.com>
2026-02-20 11:53:43 +0000

    File changes

--- src/pages/SavedData.jsx
+++ src/pages/SavedData.jsx
@@ -513,6 +513,7 @@ export default function SavedData() {
                       })}
                       </tbody>
             </table>
+            </div>
           )}
         </div>
       </div>
